b1eb62a97174aa910a52a7e55284e5f48a578b91
RettBehrens <rett@rettbehrens.codes>
2023-05-11 12:28:12 -0600

    edit sample script

--- Sample_SDK_Usage/index.js
+++ Sample_SDK_Usage/index.js
@@ -9,7 +9,7 @@ const lotr = require('rett-behrens-lotr-sdk');
     quotesAPI.accessToken = process.env.LOTR_TOKEN;
 
     const movies = await moviesAPI.getMovies();
-    console.log(movies.body.docs);
+    console.log(movies.body);
 
     // quotes will only work for the LotR trilogy, so we're filtering here
     const movieId = movies.body.docs.filter(movie => movie.name == 'The Fellowship of the Ring')[0].id;
@@ -17,16 +17,16 @@ const lotr = require('rett-behrens-lotr-sdk');
 
 
     const movie = await moviesAPI.getMovieById(movieId);
-    console.log(movie.body.docs);
+    console.log(movie.body);
 
     const quotes = await quotesAPI.getQuotes();
-    console.log(quotes.body.docs);
+    console.log(quotes.body);
 
     // In The Hobbit, the approximate distance traveled by Bilbo and his companions to Rivendell was 397 miles. It took them 38 days to get there.
     const quote = await quotesAPI.getQuoteById(quotes.body.docs[397].id);
-    console.log(quote.body.docs);
+    console.log(quote.body);
 
     // finally, quotes from specific movie
     const quotesFromTFOTR = await quotesAPI.getQuotesByMovieId(movieId);
-    console.log(quotesFromTFOTR.body.docs);
+    console.log(quotesFromTFOTR.body);
 })();
